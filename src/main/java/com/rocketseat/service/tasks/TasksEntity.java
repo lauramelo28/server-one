8188c38dedf45c5e81c4f28e09328b32f05c845e
Laura Melo <lauraenisiam@gmail.com>
2026-06-21 22:48:13 -0300

    add configs

--- src/main/java/com/rocketseat/service/tasks/TasksEntity.java
+++ src/main/java/com/rocketseat/service/tasks/TasksEntity.java
@@ -1,4 +1,25 @@
 package com.rocketseat.service.tasks;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstrutor
+@NoArgsConstructor
 public class TasksEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private LocalDateTime dueDate;
+
+    private boolean notified;
+
 }
